c03a85b9edc1582d15e61d00239bc68b4795080b
Sung Park <sungwoo.park@students.olin.edu>
2018-02-05 21:13:31 -0500

    Modify code for hw2

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -13,14 +13,24 @@ int var1;
 int main ()
 {
     int var2 = 5;
+    int var3 = 5;
     void *p = malloc(128);
+    void *q = malloc(128);
     char *s = "Literal string";
+
+    void *a = malloc(17);
+    void *b = malloc(17);
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("Address of p is %p\n", p);
     printf ("Address of s is %p\n", s);
+    printf ("Address of q is %p\n", q);
+    printf ("Address of var3 is %p\n", &var3);
+
+    printf ("a: %p\n", a);
+    printf ("b: %p\n", b);
 
     return 0;
 }
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -14,7 +14,7 @@ int *foo() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
@@ -26,7 +26,7 @@ void bar() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
@@ -45,3 +45,6 @@ int main()
 
     return 0;
 }
+
+/*
+ * You get a segmentation fault because you are returning an address to data in stack, which can be moved around the stack during the program's execution. A certain piece of data can be pushed down to lower address when there is new data pushed to a stack. It can be moved up if there are some data being popped from the stack. We see an example of this through the print statement experiment. Even though the two print statements are supposed to display an address of two different data, the address displayed is identical. This is a sign that data in the stack is being moved around. */
