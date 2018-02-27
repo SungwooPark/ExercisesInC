49e1bacc2ecd45cf0f2bb32f69feb6a082e2b193
Sung Park <sungwoo.park@students.olin.edu>
2018-02-26 22:15:44 -0500

    Add unit test exercise

---
+++ exercises/ex04/trout/test_util.c
@@ -0,0 +1,39 @@
+/* Example code for Software Systems at Olin College.
+
+Copyright 2017 Allen Downey
+License: Creative Commons Attribution-ShareAlike 3.0
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "util.h"
+#include "minunit.h"
+
+int tests_run = 0;
+
+static char *test1() {
+    int res = icmpcode_v4(1);
+    char *message = "test1 failed: icmpcode_v4(1) should return \"host unreachable\"";
+    mu_assert(message, res == "host unreachable");
+    return NULL;
+}
+
+static char * all_tests() {
+    mu_run_test(test1);
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+    char *result = all_tests();
+    if (result != NULL) {
+        printf("%s\n", result);
+    } else {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
+}
